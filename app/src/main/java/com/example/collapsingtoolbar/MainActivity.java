7691b1a6afa9c0b0eb84d76dc9185444d0274445
sourcecode121 <sourcecode121@users.noreply.github.com>
2016-10-12 14:08:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/collapsingtoolbar/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.collapsingtoolbar;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("Yoda");
+    }
+}
